b0727ba73bbe94b214060d04cbe2ed540d14e05c
hcoutino <hcoutino@gmail.com>
2019-11-08 09:12:02 -0600

    Install-Package Oracle.ManagedDataAccess.EntityFramework -Version 12.1.21

--- UAGUtileriasSAT/DataBase/PRNFACT.Designer.cs
+++ UAGUtileriasSAT/DataBase/PRNFACT.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\e562926\OneDrive - Universidad Autonoma de Guadalajara\PortalesWebHcoutino\UAGUtileriasSAT\UAGUtileriasSAT\DataBase\PRNFACT.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\e562926\OneDrive - Universidad Autonoma de Guadalajara\PortalesWebHcoutino\UtileriasSAT\UAGUtileriasSAT\DataBase\PRNFACT.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
